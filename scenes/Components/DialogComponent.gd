475ce6cd7dc8f64a52fd1332b6579f3a3e8139d1
venex-source <venex248@gmail.com>
2023-08-19 10:28:30 +0100

    Added Dialog Component

---
+++ scenes/Components/DialogComponent.gd
@@ -0,0 +1,29 @@
+extends RichTextLabel
+
+onready var tween := $Tween
+
+# KNOW: Might be used or removed in later future
+signal dialog_started
+signal dialog_changed
+signal dialog_ended
+
+export (float) var text_speed := 0.05
+export (String, FILE, "*json") var dialog_file_path
+
+var current_text
+
+func _load_dialog_file() -> Array:
+	var file = File.new()
+
+	# Check If Dialog File Exists
+	assert(file.file_exists(dialog_file_path), "Dialog File Path is Missing! Please specify a file path")
+
+	file.open(dialog_file_path, File.READ)
+
+	var dialog_text = parse_json(file.get_as_text())
+
+	return dialog_text if typeof(dialog_text) == TYPE_ARRAY else []
+
+func display_text() -> void:
+	bbcode_text = current_text
+	tween.interpolate_property(self, "percent_visible", 0.0, 1.0, len(current_text) * text_speed, tween.EASE_IN_OUT)
