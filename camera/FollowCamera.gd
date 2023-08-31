30bb8c7a8759fea83c36d079c73ba5f74d6c501a
venex-source <venex248@gmail.com>
2023-08-31 17:31:59 +0100

    Updated DragComponent

--- camera/FollowCamera.gd
+++ camera/FollowCamera.gd
@@ -4,8 +4,9 @@ export var zoom_in_limit : float = 0.5
 export var zoom_out_limit : float = 2.0
 export var zoom_speed  = 0.1
 
-onready var drag_component: Draggable
+onready var drag_component: Draggable = $Drag_n_Drop_component
 
+var is_dragging
 var zoom_level  = 1.0
 var drag_start_position
 
@@ -19,10 +20,15 @@ func _input(event):
 
 	if event.is_action_pressed("zoom_in", true):
 		zoom_level -= get_process_delta_time() * zoom_speed
-	
-	# TODO: Continuos assignment of drag_start_position. Fix
-	drag_start_position = event.position
-	drag_component.item_held_down(event)
+
+	is_dragging = drag_component.item_held_down(event)
+
+	# Validate to check for a null value
+	if is_dragging == true:
+		drag_start_position = event.position
+		set_process(true)
+	elif is_dragging == false:
+		set_process(false)
 
 	calculate_zoom()
 
@@ -36,11 +42,3 @@ func _process(delta):
 	var drag_delta = drag_start_position - get_viewport().get_mouse_position()
 	offset += drag_delta / zoom
 	drag_start_position = get_viewport().get_mouse_position()
-
-
-func _on_Drag_n_Drop_component_drag_began():
-	set_process(true)
-
-
-func _on_Drag_n_Drop_component_drag_ended():
-	set_process(false)
--- scenes/DecorativeItems/DraggableItems/DraggableItem.gd
+++ scenes/DecorativeItems/DraggableItems/DraggableItem.gd
@@ -1,23 +1,27 @@
 extends Area2D
 
+onready var timer: Timer = $Timer
 onready var drag_component: Draggable = $Drag_n_Drop_component
+
+var is_dragging
+var last_position: Vector2
 
 func _ready():
 	connect("input_event", self, "_on_BaseItem_input_event")
-	drag_component.connect("drag_began", self, "_on_Drag_n_Drop_component_drag_began")
-	drag_component.connect("drag_ended", self, "_on_Drag_n_Drop_component_drag_ended")
 	set_process(false)
 
 func _on_BaseItem_input_event(_viewport, event, _shape_idx):
-	drag_component.item_held_down(event)
+	is_dragging = drag_component.item_held_down(event)
 
+	if is_dragging != null:
+		if is_dragging:
+			last_position = global_position
+			set_process(true)
+			timer.start()
+		else:
+			set_process(false)
 
-func _on_Drag_n_Drop_component_drag_began():
-	set_process(true)
-
-func _on_Drag_n_Drop_component_drag_ended():
-	set_process(false)
 
 func _process(delta):
-	global_position = get_global_mouse_position()
-	
+	if timer.is_stopped():
+		global_position = get_global_mouse_position()
